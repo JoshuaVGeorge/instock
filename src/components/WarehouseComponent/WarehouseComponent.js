ff96b602a5059f6bd139daef91f31edc31e70164
Matthew Pellettieri <mrpellettieri@gmail.comgit config --global user.email mrpellettieri@gmail.com>
2023-11-14 17:14:02 -0800

    position of modal complete

--- src/components/WarehouseComponent/WarehouseComponent.js
+++ src/components/WarehouseComponent/WarehouseComponent.js
@@ -1,8 +1,13 @@
 import "./WarehouseComponent.scss";
+import './deleteWarehouseModal.scss';
+import { useState } from 'react';
 import arrow from "../../assets/Icons/chevron_right-24px.svg";
 import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
 import editIcon from "../../assets/Icons/edit-24px.svg";
-export default function warehouseComponent({
+import exitIcon from "../../assets/Icons/close-24px.svg";
+
+
+export default function WarehouseComponent({
   warehouseName,
   contactName,
   contactEmail,
@@ -11,10 +16,39 @@ export default function warehouseComponent({
   city,
   country,
 }) {
-  console.log(contactPhone);
+  const [modal, setModal] = useState(false);
+  const toggleModal = () => {
+    setModal(!modal)
+  } 
+  if(modal) {
+    document.body.classList.add('active--modal')
+  } else {
+    document.body.classList.remove('active--modal')
+  }
+  function DeleteWarehouseModal() {
+    return (
+        <>
+        <div className="modal">
+            <div className="overlay">
+              <div className="modal--content">
+              <div className="img--container">
+                  <button className="exit--button"><img src={exitIcon} alt="" className="exit" /></button>
+              </div>
+                <h1 className="modal__header">Delete Washington warehouse?</h1>
+                <p className="modal__text">Please confirm that you'd like to delete the Washington from the list of warehouses. You won't be able to undo this action.</p>
+                <button onClick={toggleModal} className="close--modal">Cancel</button>
+                <button className="confirm--delete">Delete</button>
+              </div>
+            </div>
+        </div>
+        </>
+    )
+  }
+  
   return (
     <>
       <section className="warehouseCard">
+        <DeleteWarehouseModal />
         <div className="warehouseCard__flex-container">
           <div className="warehouseCard__name-box">
             <div className="warehouseCard__name">
@@ -50,10 +84,11 @@ export default function warehouseComponent({
           </div>
         </div>
         <div className="warehouseCard__icons">
-          <img src={deleteIcon} alt={"delete-icon"} />
+          <button onClick={toggleModal} className="delete--button"><img src={deleteIcon} alt={"delete-icon"} /></button>
           <img src={editIcon} alt={"edit-icon"} />
         </div>
       </section>
     </>
   );
 }
+
